fb36b9d99a711b09c423f82371ef4dbf04ef3e22
Juansereina <juansereina@outlook.com>
2019-12-17 12:03:11 -0500

    fix css import

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const config = require('./config');
 const nunjucksFilters = require('./nunjucksFilters');
 
 module.exports = ((eleventyConfig) => {
+  eleventyConfig.addPassthroughCopy('css');
   nunjucksFilters(eleventyConfig);
 
   return config;
