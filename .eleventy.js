5224a2e7e1eae70c9dd566eb8785e6eaee6b3317
Juansereina <juansereina@outlook.com>
2019-12-19 14:36:36 -0500

    adding code snippet highlight

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
 const config = require('./config');
 const nunjucksFilters = require('./nunjucksFilters');
 
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = ((eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('css');
+  eleventyConfig.addPlugin(syntaxHighlight);
   nunjucksFilters(eleventyConfig);
 
   return config;
