c7d23ae4e4fe0a866f910867b8eb46ab21362c57
Juansereina <juansereina@outlook.com>
2019-12-20 11:32:40 -0500

    Copy clipboard ready

--- nunjucksFilters.js
+++ nunjucksFilters.js
@@ -5,8 +5,12 @@ const animalSection = (label, content) => `
   </div>
 `;
 
+const CopyClipboardElementWrapper = (content) =>
+(`<div onclick="handleCopyToClipboard(this)">${content}</div>`);
+
 const nunjucksFilters = (eleventyConfig) => {
   eleventyConfig.addNunjucksShortcode("animal_section", animalSection);
+  eleventyConfig.addPairedNunjucksShortcode("copy", CopyClipboardElementWrapper);
 }
 
 module.exports = nunjucksFilters;
