6f73f6fb4bac5a64414ca0c99c1fc50f063a46dc
Juansereina <juansereina@outlook.com>
2019-12-24 19:39:55 -0500

    minor changes
    Signed-off-by: Juansereina <juansereina@outlook.com>

--- __test__/test.spec.js
+++ __test__/test.spec.js
@@ -1,8 +1,8 @@
 const { importHtml, render } = require('../utils/testing');
 const SnippetSample = importHtml('index.html')
 
-describe('test', () => {
-  it('works', async () => {
+describe('SnippetSample', () => {
+  it('Should render the title correctly', async () => {
       const { getByText } = render(SnippetSample);
 
       expect(getByText('Hello world')).not.toBeEmpty()
