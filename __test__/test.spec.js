dcfd582c59b6c26818cc0dead2dafe73e57b2e7d
Juansereina <juansereina@outlook.com>
2020-01-02 09:43:03 -0500

    remove async
    Signed-off-by: Juansereina <juansereina@outlook.com>

--- __test__/test.spec.js
+++ __test__/test.spec.js
@@ -2,7 +2,7 @@ const { importHtml, render } = require('../utils/testing');
 const SnippetSample = importHtml('index.html')
 
 describe('SnippetSample', () => {
-  it('Should render the title correctly', async () => {
+  it('Should render the title correctly', () => {
       const { getByText } = render(SnippetSample);
 
       expect(getByText('Hello world')).not.toBeEmpty()
